a63e06856460908f57ca8ae9d25db3c8bf57961a
Jingze Huang <huang.jingze@columbia.edu>
2018-11-16 18:41:54 -0800

    [New] BaseWebsite.kt

---
+++ app/src/main/java/com/example/jingze/zcryptocurrency/model/websites/Website.kt
@@ -0,0 +1,6 @@
+package com.example.jingze.zcryptocurrency.model.websites
+
+enum class Website(val URL: String) {
+    BITFINEX("wss://api.bitfinex.com/ws/2"),
+    HUOBI("wss://api.huobipro.com/ws")
+}
