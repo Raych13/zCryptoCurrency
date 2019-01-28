2be2116604e8090ad866520e914dc27ee9c58400
Jingze Huang <huang.jingze@columbia.edu>
2019-01-28 00:12:01 -0800

    [New] BaseActivity and BaseFragment

---
+++ app/src/main/java/com/example/jingze/zcryptocurrency/customized/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jingze.zcryptocurrency.customized.base
+
+import dagger.android.support.DaggerAppCompatActivity
+
+class BaseActivity : DaggerAppCompatActivity()
---
+++ app/src/main/java/com/example/jingze/zcryptocurrency/customized/base/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.example.jingze.zcryptocurrency.customized.base
+
+import dagger.android.support.DaggerFragment
+
+class BaseFragment : DaggerFragment()
